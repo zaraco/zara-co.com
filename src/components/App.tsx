04f409ea5763d7d08ac08ae8a6516abd83ef1e93
Zahra Teymouri <zahrateymouri90@gmail.com>
2021-08-30 19:44:28 +0200

    architecture added

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
+import App from './components/App';
+import { store } from './store/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
