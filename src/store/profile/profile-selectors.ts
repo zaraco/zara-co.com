29463f8ce05aa0506b8b1f6ab16db654df8e85d3
Zahra Teymouri <zahrateymouri90@gmail.com>
2021-09-10 15:22:21 +0200

    profile store parameters

--- src/store/profile/profile-selectors.ts
+++ src/store/profile/profile-selectors.ts
@@ -1,8 +1,11 @@
 import { State } from "../models_d";
-import {About} from "./models_d";
+import {About, Certificate, Publication} from "./models_d";
 
 
 const profileSelectors = {
     getAbout: (state: State): About | undefined => state?.profile?.about,
+    getCertificates: (state: State): Certificate[] | undefined => state?.profile?.certificates,
+    getPublications: (state: State): Publication[] | undefined => state?.profile?.publications,
+    getLanguages: (state: State): string[] | undefined => state?.profile?.languages,
 }
 export default profileSelectors;
